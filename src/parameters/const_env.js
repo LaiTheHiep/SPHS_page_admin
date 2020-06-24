46c66558c8eb73b0d4bbf7b82bed5a29fe9ef00c
Hiep Lai <laihiep02121997@gmail.com>
2020-06-24 18:30:45 +0700

    parameter constant + lib table

---
+++ src/parameters/const_env.js
@@ -0,0 +1,22 @@
+export const db_collection = {
+  roles: 'roles',
+  users: 'users',
+  transactions: 'transactions',
+  companies: 'companies',
+  packingTickets: 'parkingtickets',
+  vehicleTypes: 'vehicletypes'
+};
+export const LINK_SPECIALS = {
+  authentication: 'authentication',
+};
+export const ADDRESS_BASE_API = 'http://localhost:8080';
+export const ROLES = {
+  admin: 'admin',
+  manager: 'manager',
+  employee: 'employee',
+  user: 'user'
+};
+export const VEHICLETYPES = {
+  car: 'car',
+  motorbike: 'motorbike'
+};
